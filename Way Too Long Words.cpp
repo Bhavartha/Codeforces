bfd852611bcf601b21d1dfcd878ec7b1abae2f5f
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-09-02 11:03:52 +0530

    Create Way Too Long Words.cpp

---
+++ Way Too Long Words.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  string s;
+  int l;
+  cin>>n;
+  while(n--){
+    cin>>s;
+    l=s.length();
+    if(l<=10) cout<<s<<endl;
+    else cout<<s[0]<<(l-2)<<s[l-1]<<endl;
+  }
+}
