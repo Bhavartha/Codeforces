099dd0ba272ab25f4e55e34759e4c587b0f65790
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-10-05 22:57:52 +0530

    Create Queue at the School.cpp

---
+++ Queue at the School.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n,t;
+  cin>>n>>t;
+  string s;
+  cin>>s;
+  while(t--){
+    for(int i=1;i<s.length();i++)
+      if(s[i-1]=='B' && s[i]=='G'){
+        swap(s[i-1],s[i]);
+        i++;
+      }
+  }
+  cout<<s<<endl;
+}
