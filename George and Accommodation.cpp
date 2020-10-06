1143b679702b3d46aef43c2ac8dff4ed26aadb29
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-10-06 12:41:46 +0530

    Create George and Accommodation.cpp

---
+++ George and Accommodation.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n,ans=0;
+  cin>>n;
+  while(n--){
+    int a,b;
+    cin>>a>>b;
+    if(b-a>=2) ans++;
+  }
+  cout<<ans<<endl;
+}
