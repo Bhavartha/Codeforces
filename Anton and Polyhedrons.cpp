1019a7aeb0bae29d4d772f892a08141e7ba0f47b
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-10-09 13:28:30 +0530

    Create Anton and Polyhedrons.cpp

---
+++ Anton and Polyhedrons.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n,ans=0;
+  cin>>n;
+  while(n--){
+    string s;
+    cin>>s;
+    if(s=="Tetrahedron") ans+=4;
+    else if(s=="Cube") ans+=6;
+    else if(s=="Octahedron") ans+=8;
+    else if(s=="Dodecahedron") ans+=12;
+    else ans+=20;
+  }
+  cout<<ans;
+}
